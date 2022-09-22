e0a7fbcca7c2e939ceb6366a0fdffa3d31d97275
Alex <newalexis777@gmail.com>
2022-09-22 13:07:52 +0400

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Показать натуральные числа от N до 1, N задано
+
+int n = 5;
+
+int SequensFor(int n)
+{
+    if(n == 1) return 1;
+    else return (n + 1) - SequensFor(n - (n-1));
+}
+
+for (int i = n; i >= 1; i--)
+{
+    Console.Write(SequensFor(i) + " ");
+}
+
