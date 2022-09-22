265b2b205ba12080f9ec6d2c912098807b695c67
Alex <newalexis777@gmail.com>
2022-09-22 17:35:09 +0400

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Найти сумму элементов от M до N, N и M заданы
+
+int n = 10;
+int m = 16;
+
+int SumMN(int x)
+{
+    if (x == m) return x;
+    else return x + SumMN(x + 1);
+}
+
+Console.WriteLine(SumMN(n));
+
+
+
+
