79e06adc55088fd6435740746e1b0184a7686adc
Alex <newalexis777@gmail.com>
2022-09-23 11:05:22 +0400

    task 6

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Написать программу вычисления функции Аккермана
-int ack(int m, int n)
+int Ackerman(int m, int n)
 {
     if (m == 0)
     {
@@ -7,19 +7,20 @@ int ack(int m, int n)
     }
     else if((m > 0) && (n == 0))
     {
-        return ack(m - 1, 1);
+        return Ackerman(m - 1, 1);
     }
     else if((m > 0) && (n > 0))
     {
-        return ack(m - 1, ack(m, n - 1));
-    }else
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+    else
     return n + 1;
 }
 
 
 void Main()
 {   
-    Console.WriteLine(ack(2, 2));
+    Console.WriteLine(Ackerman(2, 2));
 }
 
 Main();
---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿//Написать программу возведения числа А в целую стень B
+
+int Ascension(int number, int stepen) 
+{
+    if (stepen == 1) return number;
+    else if (stepen == 0) return 1;
+    else return number * Ascension(number, --stepen);
+}
+
+Console.WriteLine(Ascension(3, 2));
