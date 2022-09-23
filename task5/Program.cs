4666b9634444d39117cd8829d1aacab3d2d871b2
Alex <newalexis777@gmail.com>
2022-09-23 10:42:40 +0400

    task 5

---
+++ task5/Program.cs
@@ -0,0 +1,25 @@
+﻿//Написать программу вычисления функции Аккермана
+int ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if((m > 0) && (n == 0))
+    {
+        return ack(m - 1, 1);
+    }
+    else if((m > 0) && (n > 0))
+    {
+        return ack(m - 1, ack(m, n - 1));
+    }else
+    return n + 1;
+}
+
+
+void Main()
+{   
+    Console.WriteLine(ack(2, 2));
+}
+
+Main();
